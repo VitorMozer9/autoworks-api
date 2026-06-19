4958efa2b19e4c0d23b0ad5e8eccae5d3e297bda
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-19 03:50:59 -0300

    feat(agendamentos): Adiciona componente da página de agendamentos

---
+++ frontend/oficina/src/app/pages/agendamentos/agendamentos.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { BaseUi } from "../../components/base-ui/base-ui";
+import { NavBar } from "../../components/nav-bar/nav-bar";
+import { FormsModule } from '@angular/forms';
+import { BotaoSecundario } from "../../components/botao-secundario/botao-secundario";
+
+@Component({
+  selector: 'app-agendamentos',
+  imports: [BaseUi, NavBar, FormsModule, BotaoSecundario],
+  templateUrl: './agendamentos.html',
+  styleUrl: './agendamentos.scss',
+})
+export class Agendamentos {
+  dadosForm: any = {
+    servico: '',
+    nomeCliente: '',
+    telefone: '',
+    email: '',
+    cpf: '',
+    endereco: '',
+    placa: '',
+    modelo: '',
+    nomeMecanico: ''
+  };
+
+  // Função disparada ao clicar no botão
+  agendar() {
+    alert('Agendamento realizado com sucesso! Veja no console do navegador.');
+    console.log(this.dadosForm);
+  }
+
+  // Adicione esta função dentro da sua classe no arquivo .component.ts
+  limpar() {
+    this.dadosForm = {
+      servico: '',
+      nomeCliente: '',
+      telefone: '',
+      email: '',
+      cpf: '',
+      endereco: '',
+      placa: '',
+      modelo: '',
+      nomeMecanico: ''
+    };
+  }
+}
