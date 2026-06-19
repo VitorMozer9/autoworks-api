b760c1e039f8e1ac07dede7b03f43b926de057f6
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-19 19:04:49 -0300

    chore(app.routes.ts): Adiciona as rotas para as páginas login, home e agendamento

--- frontend/oficina/src/app/app.routes.ts
+++ frontend/oficina/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { Agendamento } from './pages/agendamento/agendamento';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    component: Login
+  },
+  {
+    path: "home",
+    component: Home
+  },
+  {
+    path: "agendamento",
+    component: Agendamento
+  },
+  {
+    path: "**",
+    redirectTo: "login"
+  }
+];
