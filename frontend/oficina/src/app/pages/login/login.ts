bab7dd6e03b4399398ef330a6e3ce7d464bc085d
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-21 12:58:55 -0300

    feat(login): Adiciona flexibilidade nos elementos da página e integração com auth-service.ts

--- frontend/oficina/src/app/pages/login/login.ts
+++ frontend/oficina/src/app/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BotaoPrimario } from '../../components/botao-primario/botao-primario';
 import { BaseUi } from "../../components/base-ui/base-ui";
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-login',
@@ -12,32 +14,48 @@ import { BaseUi } from "../../components/base-ui/base-ui";
   styleUrls: ['./login.scss']
 })
 export class Login {
-  cadastro: any = {
-    email: '',
-    senha: ''
-  };
+  cadastro = { email: '', senha: '' };
+  login = { email: '', senha: '' };
 
-  login: any = {
-    email: '',
-    senha: ''
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
   cadastrarUsuario() {
-    console.log('Enviar para a API:', this.cadastro);
-    // Chamada para o auth.service aqui
-    this.cadastro = {
-      email: '',
-      senha: ''
-    };
+    // ATUAL (Teste local)
+    this.authService.cadastrar(this.cadastro);
+    alert('Cadastro simulado com sucesso!');
+    this.cadastro = { email: '', senha: '' };
 
+    // BACKEND:
+    /*
+    this.authService.cadastrar(this.cadastro).subscribe({
+      next: () => {
+        alert('Usuário cadastrado com sucesso! Faça o login.');
+        this.cadastro = { email: '', senha: '' }; // Limpa o formulário
+      },
+      error: (erro) => console.error('Erro ao cadastrar:', erro)
+    });
+    */
   }
 
   logarUsuario() {
-    console.log("Enviar para a API: ", this.login);
-    // Chamada para o auth.service aqui
-    this.login = {
-      email: '',
-      senha: ''
-    };
+    // ATUAL (Teste local)
+    this.authService.login(this.login);
+    this.router.navigate(['/home']);
+
+    // BACKEND:
+    /*
+    this.authService.login(this.login).subscribe({
+      next: (resposta: any) => {
+        // Se a API retornar um token (ex: JWT), você pode salvar aqui
+        // localStorage.setItem('token', resposta.token);
+
+        this.router.navigate(['/home']); // Redireciona para a Home
+      },
+      error: (erro) => {
+        console.error('Erro no login:', erro);
+        alert('Email ou senha inválidos.');
+      }
+    });
+    */
   }
 }
