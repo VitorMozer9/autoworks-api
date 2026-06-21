3ac92dab08fb991643894ff3a3181e052237f5df
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-21 15:13:31 -0300

    chore(servico): Adiciona redirecionamento para página do formulário de agendamento

--- frontend/oficina/src/app/pages/servico/servico.ts
+++ frontend/oficina/src/app/pages/servico/servico.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavBar } from '../../components/nav-bar/nav-bar';
 import { BaseUi } from '../../components/base-ui/base-ui';
 import { BotaoSecundario } from '../../components/botao-secundario/botao-secundario';
@@ -12,4 +13,9 @@ import { BotaoPrimario } from '../../components/botao-primario/botao-primario';
 })
 export class Servico {
 
+  constructor(private router: Router) {}
+
+  agendar() {
+    this.router.navigate(['/novo-agendamento']);
+  }
 }
