b9f3c1daf50e5ac1d894bfc962a6c085f0f4dc11
MateusSilva06 <mateusoliver056@gmail.com>
2026-06-23 00:06:03 -0300

    fix(cadastro): Corrige erros de visualização na página de cadastro

--- frontend/oficina/src/app/pages/cadastro/cadastro.ts
+++ frontend/oficina/src/app/pages/cadastro/cadastro.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { BaseUi } from '../../components/base-ui/base-ui';
 import { NavBar } from '../../components/nav-bar/nav-bar';
-import { BotaoPrimario } from '../../components/botao-primario/botao-primario';
 import { BotaoSecundario } from '../../components/botao-secundario/botao-secundario';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [BaseUi, BotaoPrimario, BotaoSecundario, NavBar, FormsModule],
+  imports: [BaseUi, BotaoSecundario, NavBar, FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
