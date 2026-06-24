a37737fb9b9195ec8be7c8f2b27a50aa7ab65211
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-24 18:16:37 -0300

    chore(auth-services.ts): Adiciona lógica para autenticação na página de login

--- frontend/oficina/src/app/services/auth-service.ts
+++ frontend/oficina/src/app/services/auth-service.ts
@@ -1,52 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // === QUANDO O BACKEND ESTIVER PRONTO ===
-  // private apiUrl = 'http://localhost:8080/api/auth';
-  // constructor(private http: HttpClient) {}
-
-  // Simula um banco de dados temporário para o teste local
-  private usuariosMock: any[] = [];
+  // TODO: REMOVER ARRAY QUANDO O BACKEND ESTIVER CONECTADO
+  private usuariosMock: any[] = [
+    {
+      nome: 'Admin',
+      email: 'admin@oficina.com',
+      senha: 'admin',
+      areaAtuacao: 'ADMINISTRADOR'
+    }
+  ];
 
   cadastrar(dados: any) {
-    // ATUAL (Teste local): Salva no array
     this.usuariosMock.push(dados);
     console.log('Simulando envio de cadastro...', dados);
-
-    // BACKEND:
-    // return this.http.post(`${this.apiUrl}/cadastro`, dados);
   }
 
-  login(dados: any) {
-    console.log('Simulando validação de login...', dados);
+  login(dados: any): boolean {
+    const usuarioEncontrado = this.usuariosMock.find(u => u.email === dados.email && u.senha === dados.senha);
 
-    // Tenta achar o usuário no mock.
-    // Se não achar (ex: a página recarregou), pega o nome que foi digitado direto na tela de Login.
-    const usuarioEncontrado = this.usuariosMock.find(u => u.email === dados.email);
-    const nomeUsuario = usuarioEncontrado ? usuarioEncontrado.nome : (dados.nome || 'Visitante');
+    if (usuarioEncontrado) {
+      const dadosParaSalvar = {
+        nome: usuarioEncontrado.nome,
+        email: usuarioEncontrado.email,
+        areaAtuacao: usuarioEncontrado.areaAtuacao
+      };
+      localStorage.setItem('usuarioLogado', JSON.stringify(dadosParaSalvar));
+      return true;
+    }
 
-    const dadosParaSalvar = {
-      nome: nomeUsuario,
-      email: dados.email
-    };
-
-    // Salva no armazenamento do navegador
-    localStorage.setItem('usuarioLogado', JSON.stringify(dadosParaSalvar));
-
-    // BACKEND:
-    // return this.http.post(`${this.apiUrl}/login`, dados);
+    alert('Acesso Negado: Usuário não cadastrado ou senha incorreta.');
+    return false;
   }
 
-  // ==========================================
-  // FUNÇÕES PARA A NAVBAR LER OS DADOS
-  // ==========================================
   getUsuarioAtual() {
     const dados = localStorage.getItem('usuarioLogado');
-    return dados ? JSON.parse(dados) : { nome: 'Visitante', email: '' };
+    return dados ? JSON.parse(dados) : { nome: 'Visitante', email: '', areaAtuacao: '' };
   }
 
   logout() {
