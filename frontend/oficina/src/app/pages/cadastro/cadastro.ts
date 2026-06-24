5a741e111d550f7c8163a79e54084ad528ad046b
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-24 18:05:13 -0300

    refactor(cadastro): Adiciona padronização, estilização e lógica dos padrões de sintaxe do Angular na página de cadastro

--- frontend/oficina/src/app/pages/cadastro/cadastro.ts
+++ frontend/oficina/src/app/pages/cadastro/cadastro.ts
@@ -3,6 +3,7 @@ import { BaseUi } from '../../components/base-ui/base-ui';
 import { NavBar } from '../../components/nav-bar/nav-bar';
 import { BotaoSecundario } from '../../components/botao-secundario/botao-secundario';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,5 +11,39 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
+export class Cadastro {
+  dadosForm: any = {
+    nome: '',
+    telefone: '',
+    email: '',
+    cpf: '',
+    endereco: '',
+    senha: '',
+    areaAtuacao: ''
+  };
 
-export class Cadastro {}
+  constructor(private authService: AuthService) {}
+
+  cadastrar() {
+    if (!this.dadosForm.nome || !this.dadosForm.email || !this.dadosForm.senha) {
+      alert('Nome, Email e Senha são obrigatórios para o cadastro.');
+      return;
+    }
+
+    this.authService.cadastrar({ ...this.dadosForm });
+    alert('Funcionário cadastrado com sucesso!');
+    this.cancelar();
+  }
+
+  cancelar() {
+    this.dadosForm = {
+      nome: '',
+      telefone: '',
+      email: '',
+      cpf: '',
+      endereco: '',
+      senha: '',
+      areaAtuacao: ''
+    };
+  }
+}
